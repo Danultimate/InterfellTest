618472118c7b7f73f83542ed3695373f3e492d02
Daniel Blanco <dan16.daniel@gmail.com>
2019-08-08 09:41:02 -0500

    database

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=phpcrud';
-$username = 'root';
-$password = 'root';
+$dsn = 'mysql:host=https://db4free.net;dbname=phpcrud_1';
+$username = 'interfell';
+$password = 'interfell';
 $options = [];
 try {
 $connection = new PDO($dsn, $username, $password, $options);
